6cbabbb74184f44515adb8d1137216f214fd2303
Jan Schmidt <jan.schmidt@wetteronline.de>
2023-04-13 10:13:38 +0200

    .

--- Warm-Up-3/Aufgaben.cpp
+++ Warm-Up-3/Aufgaben.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 // bool equals(Bruch b1, Bruch b2){
 //     if(b1.z / b1.n == b2.z == b2.n){
 //         cout << "Die Brueche sind gleich." << endl;
@@ -10,12 +12,20 @@
 //     }
 // }
 ////------------------------------------------------------------////
-// void readRange(int &a, int start, int end)
-// {
-//     int input;
-//     do
-//     {
-//         cin >> input;
-//     } while (input < start || input > end);
-//     a = input;
-// }
+void readRange(int &a, int start, int end)
+{
+    int input;
+    do
+    {
+        cin >> input;
+    } while (input < start || input > end);
+    a = input;
+}
+
+int main()
+{
+    int a;
+    readRange(a, 1, 10);
+    cout << a << endl;
+    return 0;
+}
